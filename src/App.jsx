2aadf08e991a026546ce2d09a5c7c0b7ec008e06
Andrew Morgan <andrew.morgan@e.org>
2024-03-11 19:14:41 +0000

    Added completed collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Collaboration from './components/Collaboration'
+import Benefits from './components/Benefits'
+import Header from './components/Header'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
 
       <ButtonGradient />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
